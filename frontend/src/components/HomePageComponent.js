9ffd77831f976d86629916100d5dd8a3bd908f8b
aishwarya-suyamindra <aishwaryasr012@gmail.com>
2023-12-09 15:49:25 -0500

    Fixed merge issues

--- frontend/src/components/HomePageComponent.js
+++ frontend/src/components/HomePageComponent.js
@@ -5,6 +5,7 @@ import { Button, Container, Row, Col } from 'react-bootstrap'; // Import Bootstr
 import '../stylesheets/HomePageComponent.css';
 
 const WelcomePage = () => {
+
   return (
     <div className="welcome-page ">
       <Row className="justify-content-center align-items-center min-vh-100">
@@ -12,18 +13,18 @@ const WelcomePage = () => {
           <h1>Welcome to Your Streaming Service</h1>
           <p className="lead">Unlimited access to thousands of movies and TV shows.</p>
 
-          <div className="cta-buttons">
-            <Link to="/register">
-              <Button variant="primary" className="me-2">
-                Register
-              </Button>
-            </Link>
+            <div className="cta-buttons">
+              <Link to="/register">
+                <Button variant="primary" className="me-2">
+                  Register
+                </Button>
+              </Link>
 
-            <Link to="/login">
-              <Button variant="secondary" className="me-2">
-                Login
-              </Button>
-            </Link>
+              <Link to="/login">
+                <Button variant="secondary" className="me-2">
+                  Login
+                </Button>
+              </Link>
 
             <Link to="/browse">
               <Button variant="success">Continue as Guest</Button>
