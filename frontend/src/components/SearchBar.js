d76e6a0788adacc0e061a4a615aa87356d3ff538
sushruth-06 <sushruthkonapur@gmail.com>
2023-12-11 23:55:07 -0500

    search result page and comment

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 import { Form, FormControl, Button, InputGroup, ListGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from "react-router-dom";
+
 
 const SearchBar = () => {
+  const navigate = useNavigate()
   const BASE_REMOTE_URL = "http://localhost:3500" // move this to env.
   const [searchText, setSearchText] = useState('');
   const [videoData, setVideoData] = useState([]);
+
   const handleSearch = () => {
     console.log('Searching for:', searchText);
      // Call the remote API with searchText
@@ -19,6 +23,11 @@ const SearchBar = () => {
       }
       )
       .catch(error => console.error('Error fetching video data:', error));
+  };
+
+  const handleListItemClick = (videoId,isUploadedVideo) => {
+    console.log(isUploadedVideo)
+    navigate(`../video/${videoId}`, {state: {isUploadedVideo: isUploadedVideo}})
   };
 
   return (
@@ -38,12 +47,16 @@ const SearchBar = () => {
     </Form>
     {videoData.length > 0 && (
         <ListGroup>
-          {videoData.map(video => (
-            <ListGroup.Item key={video.videoId}>
-              {video.title}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
+        {videoData.map(video => (
+          <ListGroup.Item
+            key={video.videoId}
+            action
+            onClick={() => handleListItemClick(video.videoId,video.isUploadedVideo)}
+          >
+            {video.title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
       )}
     </div>
   );
